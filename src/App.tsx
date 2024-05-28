1a90c4ba8341c4f846706b70780437a1a2ec196b
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2024-05-28 19:27:56 +0800

    feat:🏳️‍🌈 base status

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,45 @@ function App() {
     colors: pride_color,
     num: 10,
   });
+  const [gap, setGap] = useState<number>(2);
+  const [size, setSize] = useState<number>(4);
   return (
     <main>
       <header>Pride</header>
-      <Flag colors={pride.colors} num={pride.num} />
+      <label htmlFor="gap" style={{ display: "flex", flexDirection: "row" }}>
+        <strong>GAP:</strong>
+        <input
+          id="gap"
+          type="range"
+          name="gap"
+          max="4"
+          min="0"
+          value={gap}
+          onChange={(event) => {
+            if (!event.target.value) return;
+            setGap(Number(event.target.value));
+          }}
+        />
+        {gap}
+      </label>
+
+      <label htmlFor="size" style={{ display: "flex", flexDirection: "row" }}>
+        <strong>SIZE:</strong>
+        <input
+          id="size"
+          type="range"
+          name="size"
+          value={size}
+          max="6"
+          min="1"
+          onChange={(event) => {
+            if (!event.target.value) return;
+            setSize(Number(event.target.value));
+          }}
+        />
+        {size}
+      </label>
+      <Flag size={size} gap={gap} colors={pride.colors} num={pride.num} />
     </main>
   );
 }
--- src/compontents/Flag.tsx
+++ src/compontents/Flag.tsx
@@ -1,14 +1,26 @@
 import { Row } from "../constant/pride.constant";
 
 import "./Flag.css";
+interface FlagProps {
+  gap: number;
+  size: number;
+}
 
-const Flag = <T,>({ colors, num }: Row<T>) => {
-  const transMatrices = (
-    rows: Row<T>["colors"]
-  ): { color: string; id: number }[][] => {
+const Flag = <T,>({ colors, num, size = 4, gap = 2 }: FlagProps & Row<T>) => {
+  //   const transMatrices = (
+  //     rows: Row<T>["colors"]
+  //   ): { color: string; id: number }[][] => {
+  //     let currId = 1;
+  //     return rows.map((color) =>
+  //       new Array(num).fill(null).map(() => {
+  //         return { color, id: currId++ };
+  //       })
+  //     );
+  //   };
+  const transMatrices = (rows: Row<T>["colors"]) => {
     let currId = 1;
-    return rows.map((color) =>
-      new Array(num).fill(null).map(() => {
+    return [1, 2].map(() =>
+      rows.map((color) => {
         return { color, id: currId++ };
       })
     );
@@ -16,27 +28,39 @@ const Flag = <T,>({ colors, num }: Row<T>) => {
   return (
     <div
       style={{
-        maxHeight: "400px",
-        borderRadius: "3px",
-        overflow: "hidden",
+        margin: "20px 0",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
       {transMatrices(colors).map((row, idx) => (
         <div
           className="row"
           key={idx}
-          style={{ display: "flex", flexDirection: "row" }}
+          style={{
+            display: "flex",
+            gap: `${gap * 0.25}rem`,
+            flexDirection: "row",
+
+            transition: "all 1s  cubic-bezier(.5, -1, .5, 2)",
+          }}
         >
-          {row.map((color) => (
+          {row.map((color, colorIdx) => (
             <div
               className="row_item"
               key={`${color.id}`}
-              style={{
-                width: "80px",
-                height: "20px",
-                animationDelay: `${color.id + 1}s`,
-                backgroundColor: `${color.color}`,
-              }}
+              style={
+                {
+                  width: "calc(var(--size) * 1rem)",
+                  height: "calc(var(--size) * 1rem)",
+                  marginBottom: `${gap * 0.25}rem`,
+                  animationDelay: `${(colorIdx + 1) * 0.2}s`,
+                  transition: "all 1s  cubic-bezier(.5, -1, .5, 2)",
+                  backgroundColor: `${color.color}`,
+                  "--size": size * 0.25,
+                } as React.CSSProperties
+              }
             ></div>
           ))}
         </div>
