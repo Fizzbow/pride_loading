9ed1c507d97a00077de7e04915d0d70b4e8945b8
Fizzbow <55476631+Fizzbow@users.noreply.github.com>
2024-08-06 19:54:47 +0800

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,4 @@
-import { useState } from "react";
-
 import "./App.css";
-
-import {
-  PrideColorType,
-  Row,
-  PRIDE_COLOR,
-  LGBT_COLOR,
-  // TRANS_COLOR,
-} from "./constant/pride.constant";
-import Wave from "./components/Wave";
-import Bar from "./components/Bar";
-import { MagicRainbowButton } from "./components/RainbowBtn";
 import { PrideProgress } from "./components/PrideProgress";
 
 function App() {
@@ -24,65 +11,10 @@ function App() {
       }}
     >
       <header>Pride</header>
-      {/* <RowsLoadingGroup colors={PRIDE_COLOR} />
-      <FlagControlGroup colors={PRIDE_COLOR} /> */}
-      {/* <RowsLoadingGroup colors={TRANS_COLOR} /> */}
-      {/* <MagicRainbowButton /> */}
+
       <PrideProgress />
     </main>
   );
 }
-
-const RowsLoadingGroup = ({ colors }: { colors: PrideColorType[] }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [pride, _] = useState<Row<PrideColorType>>({
-    colors,
-    num: 10,
-  });
-  const [gap, setGap] = useState<number>(2);
-  const [size, setSize] = useState<number>(4);
-  return (
-    <section>
-      <label htmlFor="gap" style={{ display: "flex", flexDirection: "row" }}>
-        <strong>GAP:</strong>
-        <input
-          id="gap"
-          type="range"
-          name="gap"
-          max="4"
-          min="0"
-          value={gap}
-          onChange={(event) => {
-            if (!event.target.value) return;
-            setGap(Number(event.target.value));
-          }}
-        />
-        {gap}
-      </label>
-
-      <label htmlFor="size" style={{ display: "flex", flexDirection: "row" }}>
-        <strong>SIZE:</strong>
-        <input
-          id="size"
-          type="range"
-          name="size"
-          value={size}
-          max="6"
-          min="1"
-          onChange={(event) => {
-            if (!event.target.value) return;
-            setSize(Number(event.target.value));
-          }}
-        />
-        {size}
-      </label>
-      <Wave size={size} gap={gap} colors={pride.colors} num={pride.num} />
-    </section>
-  );
-};
-
-const FlagControlGroup = ({ colors }: { colors: PrideColorType[] }) => {
-  return <Bar num={2} colors={LGBT_COLOR} />;
-};
 
 export default App;
